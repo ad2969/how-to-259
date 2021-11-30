12bc35616f44de81206cb0cd992be708d80d09b7
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-11-30 11:46:53 -0800

    remove intermediary 2d

--- lab5/CPSC259_Lab5_Project/CPSC259_Lab5/pagerank.c
+++ lab5/CPSC259_Lab5_Project/CPSC259_Lab5/pagerank.c
@@ -17,7 +17,7 @@ void print_ml_matrix(mxArray* matrix, int dimensions) {
 
 	for (j = 0; j < size / dimensions; ++j) {
 		for (i = 0; i < size / dimensions; ++i) {
-			printf("%f ", *(mxGetPr(matrix) + (i * dimensions) + j));
+			printf("%.0f ", *(mxGetPr(matrix) + (i * dimensions) + j));
 		}
 		printf("\n");
 	}
@@ -36,13 +36,7 @@ void get_connectivity_matrix(FILE* filePointer, int* dimensions, double*** Conne
 	}
 
 	// initialize the connectivity matrix
-	*ConnectivityMatrix = (double**)malloc(sizeof(double*) * (*dimensions));
-	for (i = 0; i < (*dimensions); ++i) {
-		(*ConnectivityMatrix)[i] = (double*)malloc(sizeof(double) * (*dimensions));
-	}
-
-	i = 0;
-	j = 0;
+	*ConnectivityMatrix = mxCreateDoubleMatrix(*dimensions, *dimensions, mxREAL);
 
 	// get to the top of the file and start reading
 	fseek(filePointer, 0, SEEK_SET);
@@ -52,9 +46,14 @@ void get_connectivity_matrix(FILE* filePointer, int* dimensions, double*** Conne
 			j++;
 		}
 		else if (ch != ' ') {
-			(*ConnectivityMatrix)[i][j] = (int)ch - 48;
+			mxGetPr(*ConnectivityMatrix)[i * (*dimensions) + j] = (double)ch - 48;
 			i++;
 		}
+	}
+
+	if (j != *dimensions - 1) {
+		fprintf(stderr, "The provided matrix is not square (#rows = #columns)");
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -67,13 +66,11 @@ int main(void) {
 	Engine* ep = NULL;
 	FILE* filePointer = NULL;
 
-	double** ConnectivityMatrix = NULL;
-	mxArray* MXConnectivityMatrix = NULL;
-	char** PageRank = NULL;
-	mxArray* MXPageRank = NULL;
+	mxArray* ConnectivityMatrix = NULL;
+	mxArray* PageRank = NULL;
 
 	// open the file
-	printf("Loading file %s\n", FILE_NAME);
+	printf("Loading file '%s'...\n", FILE_NAME);
 	error = fopen_s(&filePointer, FILE_NAME, "r");
 	if (error != 0) {
 		fprintf(stderr, "File %s cannot be loaded\n", FILE_NAME);
@@ -82,114 +79,94 @@ int main(void) {
 
 	// get connectivity matrix
 	get_connectivity_matrix(filePointer, &dimensions, &ConnectivityMatrix);
+	printf("\nLoaded %i x %i connectivity matrix:\n", dimensions, dimensions);
+	print_ml_matrix(ConnectivityMatrix, dimensions);
 
+	// connect to matlab engine
 	if (!(ep = engOpen(NULL))) {
 		fprintf(stderr, "\nCan't start MATLAB engine\n");
 		exit(EXIT_FAILURE);
 	}
-
-	/* Create buffer string for automatically capturing MATLAB engine output */
 	if (engOutputBuffer(ep, buffer, BUFSIZE)) {
 		fprintf(stderr, "\nCan't create buffer for MATLAB output\n");
 		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < dimensions; ++i) {
-		for (int j = 0; j < dimensions; ++j) {
-			printf("%f ", ConnectivityMatrix[i][j]);
-		}
-		printf("\n");
+	// save variables to matlab
+	if (engPutVariable(ep, "ConnectivityMatrix", ConnectivityMatrix)) {
+		fprintf(stderr, "\nCannot write connectivity matrix to MATLAB\n");
+		exit(EXIT_FAILURE);
 	}
-	printf("\n\n\n");
-
-	// store the connectivity matrix in matlab (need to switch rows and columns)
-	MXConnectivityMatrix = mxCreateDoubleMatrix(dimensions, dimensions, mxREAL);
-	for (int i = 0; i < dimensions; ++i) {
-		for (int j = 0; j < dimensions; ++j) {
-			memcpy(&mxGetPr(MXConnectivityMatrix)[i * dimensions + j], &ConnectivityMatrix[j][i], sizeof(double));
-		}
+	mxArray* MXDimensions = mxCreateDoubleScalar(dimensions);
+	if (engPutVariable(ep, "dimension", MXDimensions)) {
+		fprintf(stderr, "\nCannot write dimension variable to MATLAB\n");
+		exit(EXIT_FAILURE);
 	}
-	memcpy((void*)mxGetPr(MXConnectivityMatrix), (void*)ConnectivityMatrix, dimensions * sizeof(double));
-	printf("Read a %i x %i connectivity matrix:\n", dimensions, dimensions);
-	print_ml_matrix(MXConnectivityMatrix, dimensions);
-	if (engPutVariable(ep, "ConnectivityMatrix", MXConnectivityMatrix)) {
-		fprintf(stderr, "\nCannot write matrix one to MATLAB\n");
+	mxArray* MXPF = mxCreateDoubleScalar(PROBABILITY_FACTOR);
+	if (engPutVariable(ep, "pf", MXPF)) {
+		fprintf(stderr, "\nCannot write pf variable to MATLAB\n");
 		exit(EXIT_FAILURE);
 	}
 
-	system("PAUSE");
+	// do the matlab calculations
+	if (engEvalString(ep, "columnsums = sum(ConnectivityMatrix, 1); zerocolumns = find(columnsums ~= 0);")) {
+		fprintf(stderr, "\nError getting connectivity matrix parameters\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "D = sparse(zerocolumns, zerocolumns, 1./columnsums(zerocolumns), dimension, dimension);")) {
+		fprintf(stderr, "\nError creating sparse matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "StochasticMatrix = ConnectivityMatrix * D;")) {
+		fprintf(stderr, "\nError calculating stochastic matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "[row, column] = find(columnsums == 0); StochasticMatrix(:, column) = 1./dimension;")) {
+		fprintf(stderr, "\nError filling stochastic matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "Q = ones(dimension, dimension);")) {
+		fprintf(stderr, "\nError creating scaling matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "TransitionMatrix = pf * StochasticMatrix + (1 - pf) * (Q/dimension);")) {
+		fprintf(stderr, "\nError calculating transition matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "PageRank = ones(dimension, 1);")) {
+		fprintf(stderr, "\nError initializing pagerank\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "for i = 1:100 PageRank = TransitionMatrix * PageRank; end")) {
+		fprintf(stderr, "\nError calculating pagerank\n");
+		exit(EXIT_FAILURE);
+	}
+	if (engEvalString(ep, "PageRank = PageRank / sum(PageRank)")) {
+		fprintf(stderr, "\nError normalizing pagerank\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// get matlab results
+	if ((PageRank = engGetVariable(ep, "PageRank")) == NULL) {
+		fprintf(stderr, "\nFailed to retrieve result matrix\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("\nNODE  RANK");
+	printf("\n---   ----\n");
+	for (int i = 0; i < dimensions; ++i) {
+		printf("%i     %.4f\n", i + 1, mxGetPr(PageRank)[i]);
+	}
+	printf("\n");
 
 	// cleaning and closing matlab engine
-	mxDestroyArray(MXConnectivityMatrix);
-	mxDestroyArray(MXPageRank);
-	free(ConnectivityMatrix);
-	free(PageRank);
-
-	MXConnectivityMatrix = NULL;
-	MXPageRank = NULL;
-	ConnectivityMatrix = NULL;
-	PageRank = NULL;
+	mxDestroyArray(ConnectivityMatrix);
+	mxDestroyArray(PageRank);
 
 	if (engClose(ep)) {
 		fprintf(stderr, "\nFailed to close MATLAB engine\n");
 	}
 
 	system("PAUSE");
-	return 1;
-
-	/*
-
-	if (engPutVariable(ep, "matrixOne", matrixOne)) {
-		fprintf(stderr, "\nCannot write matrix one to MATLAB\n");
-		system("pause");
-		exit(1);
-	}
-	if (engPutVariable(ep, "matrixTwo", matrixTwo)) {
-		fprintf(stderr, "\nCannot write matrix two to MATLAB\n");
-		system("pause");
-		exit(1);
-	}
-
-	if (engEvalString(ep, "resultMatrix = matrixOne * matrixTwo")) {
-		fprintf(stderr, "\nError multiplying matrices\n");
-		system("pause");
-		exit(1);
-	}
-
-	if ((result = engGetVariable(ep, "resultMatrix")) == NULL) {
-		fprintf(stderr, "\nFailed to retrieve result matrix\n");
-		system("pause");
-		exit(1);
-	}
-	else {
-		size_t i = 0;
-		size_t j = 0;
-
-		printf("The first matrix was:\n");
-		printMLMatrix(matrixOne, DIMENSIONS);
-
-		printf("The second matrix was:\n");
-		printMLMatrix(matrixTwo, DIMENSIONS);
-
-		printf("The matrix product is:\n");
-		printMLMatrix(result, DIMENSIONS);
-	}
-
-	engEvalString(ep, "whos");
-	printf("%s\n", buffer);
-
-	mxDestroyArray(matrixOne);
-	mxDestroyArray(matrixTwo);
-	mxDestroyArray(result);
-	matrixOne = NULL;
-	matrixTwo = NULL;
-	result = NULL;
-
-	if (engClose(ep)) {
-		fprintf(stderr, "\nFailed to close MATLAB engine\n");
-	}
-
-	system("pause");
 	return 0;
-	*/
 }
